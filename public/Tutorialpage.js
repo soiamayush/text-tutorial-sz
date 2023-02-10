f8591f2cd5c561d476a89252ff58e27cbdbfe244
ayush bhargava <bhargava.ayush71@gmail.com>
2023-02-10 15:55:46 +0530

    hero section changed

--- public/Tutorialpage.js
+++ public/Tutorialpage.js
@@ -4,32 +4,21 @@ const h1 = document.getElementById("h1"),
 
 const onScroll = (event) => {
   const scrollPosition = event.target.scrollingElement.scrollTop;
-
   if (scrollPosition > 150) {
     banner.style.backgroundSize = "150%";
-
     h1.style.opacity = 0;
-
-    h1.style.translate = "0-50px";
-
-    hl.style.scale = "0.9";
-
+    h1.style.translate = "0 -50px";
+    h1.style.scale = "0.9";
     button.style.opacity = 0;
-
-    button.style.translate = "0 -40px";
+    button.style.translate = "0 -50px";
     button.style.scale = "0.8";
   } else {
     banner.style.backgroundSize = "180%";
-
     h1.style.opacity = 1;
-    hl.style.translate = 0;
-
+    h1.style.translate = 0;
     h1.style.scale = 1;
-
     button.style.opacity = 1;
-
     button.style.translate = 0;
-
     button.style.scale = 1;
   }
 };
